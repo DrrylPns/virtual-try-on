b980ef0848c998de7f073a8bad4afbdbd1c3c9e7
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2025-06-06 16:16:48 +0800

    feat: proper models

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -11,6 +11,199 @@ export const variants = [
     variant: "Glass B",
     path: "/models/model1-center.glb",
   },
+
+  //Cove
+  {
+    model: "Cove",
+    variant: "Pandan",
+    path: "/rescaled-models/Cove/pandan.glb",
+  },
+  {
+    model: "Cove",
+    variant: "Petal",
+    path: "/rescaled-models/Cove/petal.glb",
+  },
+  {
+    model: "Cove",
+    variant: "Rich Black",
+    path: "/rescaled-models/Cove/rich-black.glb",
+  },
+  {
+    model: "Cove",
+    variant: "Space Gray",
+    path: "/rescaled-models/Cove/space-gray.glb",
+  },
+
+  // Benett
+  {
+    model: "Bennett",
+    variant: "Ash",
+    path: "/rescaled-models/Bennett/ash.glb",
+  },
+  {
+    model: "Bennett",
+    variant: "Honey Tort",
+    path: "/rescaled-models/Bennett/honey-tort.glb",
+  },
+  {
+    model: "Bennett",
+    variant: "Matcha",
+    path: "/rescaled-models/Bennett/matcha.glb",
+  },
+  {
+    model: "Bennett",
+    variant: "Rich Black",
+    path: "/rescaled-models/Bennett/rich-black.glb",
+  },
+
+  // Leto
+  {
+    model: "Leto",
+    variant: "Quartz",
+    path: "/rescaled-models/Leto/quartz.glb",
+  },
+  {
+    model: "Leto",
+    variant: "Rich Black",
+    path: "/rescaled-models/Leto/rich-black.glb",
+  },
+  {
+    model: "Leto",
+    variant: "Stone",
+    path: "/rescaled-models/Leto/stone.glb",
+  },
+
+  // Lana
+  {
+    model: "Lana",
+    variant: "Amber Tort",
+    path: "/rescaled-models/Lana/amber-tort.glb",
+  },
+  {
+    model: "Lana",
+    variant: "Mint",
+    path: "/rescaled-models/Lana/mint.glb",
+  },
+  {
+    model: "Lana",
+    variant: "Rich Black",
+    path: "/rescaled-models/Lana/rich-black.glb",
+  },
+  {
+    model: "Lana",
+    variant: "Space Gray",
+    path: "/rescaled-models/Lana/space-gray.glb",
+  },
+
+  // Jax
+  {
+    model: "Jax",
+    variant: "Honey Tort",
+    path: "/rescaled-models/Jax/honey-tort.glb",
+  },
+  {
+    model: "Jax",
+    variant: "Peony",
+    path: "/rescaled-models/Jax/peony.glb",
+  },
+  {
+    model: "Jax",
+    variant: "Pickle",
+    path: "/rescaled-models/Jax/pickle.glb",
+  },
+  {
+    model: "Jax",
+    variant: "Rich Black",
+    path: "/rescaled-models/Jax/rich-black.glb",
+  },
+  // Lindy
+  {
+    model: "Lindy",
+    variant: "Cherry",
+    path: "/rescaled-models/Lindy/cherry.glb",
+  },
+  {
+    model: "Lindy",
+    variant: "Chestnut",
+    path: "/rescaled-models/Lindy/chestnut.glb",
+  },
+  {
+    model: "Lindy",
+    variant: "Horchata",
+    path: "/rescaled-models/Lindy/horchata.glb",
+  },
+  {
+    model: "Lindy",
+    variant: "Lemonade",
+    path: "/rescaled-models/Lindy/lemonade.glb",
+  },
+  {
+    model: "Lindy",
+    variant: "Milk",
+    path: "/rescaled-models/Lindy/milk.glb",
+  },
+  {
+    model: "Lindy",
+    variant: "Pear",
+    path: "/rescaled-models/Lindy/pear.glb",
+  },
+  {
+    model: "Lindy",
+    variant: "Plum",
+    path: "/rescaled-models/Lindy/plum.glb",
+  },
+  {
+    model: "Lindy",
+    variant: "Rich Black",
+    path: "/rescaled-models/Lindy/rich-black.glb",
+  },
+  {
+    model: "Lindy",
+    variant: "Smoke",
+    path: "/rescaled-models/Lindy/smoke.glb",
+  },
+  // Lou
+  {
+    model: "Lou",
+    variant: "Cherry",
+    path: "/rescaled-models/Lou/cherry.glb",
+  },
+  {
+    model: "Lou",
+    variant: "Jelly",
+    path: "/rescaled-models/Lou/jelly.glb",
+  },
+  {
+    model: "Lou",
+    variant: "Rich Black",
+    path: "/rescaled-models/Lou/rich-black.glb",
+  },
+  {
+    model: "Lou",
+    variant: "Smoke",
+    path: "/rescaled-models/Lou/smoke.glb",
+  },
+  // Elba
+  {
+    model: "Elba",
+    variant: "Almond",
+    path: "/rescaled-models/Elba/almond.glb",
+  },
+  {
+    model: "Elba",
+    variant: "Cherry",
+    path: "/rescaled-models/Elba/cherry.glb",
+  },
+  {
+    model: "Elba",
+    variant: "Jelly",
+    path: "/rescaled-models/Elba/jelly.glb",
+  },
+  {
+    model: "Elba",
+    variant: "Rich Black",
+    path: "/rescaled-models/Elba/rich-black.glb",
+  },
 ];
 
 export const models = [
@@ -39,7 +232,7 @@ export const models = [
     image: Sample,
   },
   {
-    name: "Lato",
+    name: "Leto",
     image: Sample,
   },
   {
